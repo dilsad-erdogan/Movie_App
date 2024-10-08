f123bd89b272a7857b2c77da9ff07d7403e06c26
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-10-08 19:48:41 +0300

    fav page

--- frontend/src/components/Fav/Fav.jsx
+++ frontend/src/components/Fav/Fav.jsx
@@ -1,11 +1,40 @@
+import { useEffect, useState } from "react";
 import FavCard from "./FavCard"
 
 const Fav = () => {
+  const [trends, setTrends] = useState([]);
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZjVlMzljZjhjMjFkZWI4NDdjNmVhZGNkNzg0NDNlZCIsIm5iZiI6MTcyODMxNjc2MS4xNTAxOTQsInN1YiI6IjY3MDQwMjg4M2Q3YjNjNmMwNzc5NjkyYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FW1XjHUlaKWx0gtgxSvHc_tIja7jk-GqhsGHTNQ6lF0'
+    }
+  };
+
+  useEffect(() => {
+    const day = new Date();
+    fetch(`https://api.themoviedb.org/3/trending/all/day?query=${day}&language=en-US`, options)
+        .then(response => response.json())
+        .then(response => {
+            setTrends(response.results || []);
+        })
+    .catch(err => console.error(err));
+  }, []);
+
   return (
-    <div className="flex gap-5">
-        <FavCard fav={"Avengers"} />
-        <FavCard fav={"Black Swan"} />
-        <FavCard fav={"Titanic"} />
+    <div className="container mx-auto">
+      <div className="overflow-hidden min-h-screen">
+        <div className="mt-14">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 place-items-center mt-8">
+            {trends.map((trend) => (
+              <div key={trend.id}>
+                <FavCard fav={trend}/>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
