bf6d28c0bae527cd20f1d981ca6adb5936de365d
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-10-09 20:56:17 +0300

    fav add update

--- backend/controllers/fav.js
+++ backend/controllers/fav.js
@@ -17,12 +17,23 @@ async function getFav(req, res) {
 };
 
 async function addFav(req, res) {
-    try{
+    try {
         const { user_id, movie_id } = req.body;
 
-        const id = await User.findById(user_id);
-        if(!id) {
+        const user = await User.findById(user_id);
+        if (!user) {
             return res.status(400).json({ success: false, message: 'User not found!' });
+        }
+
+        const existingFav = await Fav.findOne({ user_id: user_id, movie_id: movie_id });
+        if (existingFav) {
+            if (existingFav.is_active) {
+                return res.status(400).json({ success: false, message: 'This movie is already in your favorites!' });
+            } else {
+                existingFav.is_active = true;
+                const updatedFav = await existingFav.save();
+                return res.status(200).json({ success: true, message: 'Favorite re-activated', data: updatedFav });
+            }
         }
 
         const fav = new Fav({
@@ -32,12 +43,12 @@ async function addFav(req, res) {
         });
 
         const savedFav = await fav.save();
-        if(savedFav) {
+        if (savedFav) {
             res.status(201).json({ success: true, data: savedFav });
         } else {
             res.status(400).json({ success: false, message: 'Fav error!' });
         }
-    } catch(error) {
+    } catch (error) {
         res.status(500).json({ error: 'Internal server error!' });
     }
 };
